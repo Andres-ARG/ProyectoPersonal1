6a1eb20aa468fdc8559cb1050f2c896cb348ef61
Andres-ARG <andrescamposb@gmail.com>
2023-05-19 15:53:07 -0300

    First Commit

---
+++ src/components/Timer.jsx
@@ -0,0 +1,43 @@
+import { CircularProgressbar, buildStyles  } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import PlayButton from './PlayButton';
+import PauseButton from './PauseButton';
+import SettingsButton from './SettingsButton';
+import { useContext, useState, useEffect } from 'react';
+import SettingsContext from '../context/SettingsContext';
+
+const red = "#f54e4e"
+const green = "#4aec8c"
+
+const Timer = () => {
+  const settingsInfo = useContext(SettingsContext)
+  
+  const [isPaused, setIsPaused] = useState(true)
+  const [secondsLeft, setSecondsLeft] = useState(0)
+
+  function initTimer() {
+    setSecondsLeft(settingsInfo.workMinutes * 60)
+  }
+
+  useEffect(() => {
+
+  }, [settingsInfo])
+
+  return (
+    <div>
+        <CircularProgressbar value={60} text={`${60}%`} styles={buildStyles({
+            textColor: "#fff",
+            pathColor: red,
+            trailColor:"rgba(255,255,255,.2)"
+        })} />
+        <div style={{marginTop:"20px"}}>
+          {isPaused ? <PlayButton /> : <PauseButton />}
+        </div>
+        <div style={{marginTop:"20px"}}>
+          <SettingsButton onClick={() => settingsInfo.setShowSettings(true)}/>
+        </div>
+    </div>
+  )
+}
+
+export default Timer
